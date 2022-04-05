1d5b4c5e25bef1151372b3f431fa5d3e66db3585
Gaurav <gaurav.rpandit1@gmail.com>
2022-04-05 22:57:11 +0530

    Made Some changes in server.js

--- DiscussionForum/server.js
+++ DiscussionForum/server.js
@@ -1,6 +1,15 @@
+const { Socket } = require('engine.io');
 const express = require('express');
+const http = require('http');
+const socketio = require('socket.io')
+
 const app = express();
+const server = http.createServer(app); 
+const io = socketio(server);
+
 const PORT = 3000 || process.env.PORT;
+io.on('connection', Socket=>{
+    console.log("New Ws Connection..")
+});
 
-
-app.listen(PORT, `Server running of port number: ${PORT}`);
+server.listen(PORT, () =>{ console.log(`Server running of port number: ${PORT}`);});
